ad962e0242f75b818fb250001512152de573bed8
Khaled-Ash <77514164+Khaled-Ash@users.noreply.github.com>
2021-01-22 15:35:38 -0500

    PA2

--- MDCode/InitVStream.m
+++ MDCode/InitVStream.m
@@ -15,7 +15,9 @@ PhiCutoff = 3 * AtomSpacing * 1.1;
 
 T = 30;
 
-AddRectAtomicArray(10, 10, 0, 0, 0, 0, 0, T, 0);
+%AddRectAtomicArray(10, 10, 0, 0, 0, 0, 0, T, 0);
+%AddCircAtomicArray(7, 0, 0, 0, 0, 0, T, 0);
+AddEllipticalAtomicArray(9, 5, 0, 0, 0, 0, 0, T, 0);
 % vy0 = -sqrt(0.02*Ep/Mass1);
 % AddRectAtomicArray(4,4,0,12*AtomSpacing,0,vy0,0,T,1);
 Ep = 2;
